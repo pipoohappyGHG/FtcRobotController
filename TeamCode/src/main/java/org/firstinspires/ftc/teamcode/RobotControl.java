fcbe450d190b2d61157f3d429f3a9c4a1705f6cd
pipoohappyGHG <xaver.aisenpreis@icloud.com>
2025-11-18 10:38:49 -0800

    added first code
    added first code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotControl.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+@TeleOp
+public class RobotControl extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Goodnight", "Derek");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
