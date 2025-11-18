5f74416b515fc8ced7daa8e3441b79faf302ecb5
pipoohappyGHG <xaver.aisenpreis@icloud.com>
2025-11-18 11:43:05 -0800

    Update RobotControl.java
    Added first class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotControl.java
@@ -1,18 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 
+@Disabled
 @TeleOp
 public class RobotControl extends OpMode {
     @Override
     public void init() {
-        telemetry.addData("Goodnight", "Derek");
+        telemetry.addData("Goodnight", "Derek"); // Love you Derek
     }
 
     @Override
     public void loop() {
+
+        // This is an example of a single line comment.
+        // The computer completely ignores these.
+
+        /*
+         Anything between this astrict
+        and the slash
+        will be completely ignored.
+        */
 
     }
 }
