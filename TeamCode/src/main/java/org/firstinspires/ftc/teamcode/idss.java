19fefef0c2d36231321886c252723853501da6c9
pipoohappyGHG <xaver.aisenpreis@icloud.com>
2025-11-19 10:14:48 -0800

    deleted 2 files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/idss.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-public class idss {
-}
-
-
-
-if uoou si c0sadufc9
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testtestpls.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-@TeleOp
-public class testtestpls extends OpMode {
-    testtestpls;
-}
