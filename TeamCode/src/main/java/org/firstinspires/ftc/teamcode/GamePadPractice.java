405d0c44c75c86cb39483bdf57f99ae34464e66b
pipoohappyGHG <xaver.aisenpreis@icloud.com>
2025-11-19 10:04:21 -0800

    Update GamePadPractice.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-@Disabled
+
 @TeleOp
 
 public class GamePadPractice extends OpMode {
