ccaae0a6571ecf6cbc805dcba40b8650c908b14b
pipoohappyGHG <xaver.aisenpreis@icloud.com>
2025-11-18 17:52:05 -0800

    first home test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UltimativeTest.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+public class UltimativeTest extends LinearOpMode {
+
+}
